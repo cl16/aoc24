28138328ba2c5bc0b1c5883d4c139fcbbfc4f605
Collin Lacoursiere <61947934+cl16@users.noreply.github.com>
2024-12-08 14:50:41 -0500

    adds day 2 part 2

--- src/main/java/org/solution/Day2.java
+++ src/main/java/org/solution/Day2.java
@@ -8,54 +8,7 @@ import java.util.List;
 public class Day2 {
 
     public static Integer solve() {
-
-        List<String> input = InputReader.textFile("data/day2.txt");
-        List<List<Integer>> reports = extractReports(input);
-        int safeReports = 0;
-        List<Integer> report;
-
-        for (int i = 0; i < reports.size(); i++) {
-            report = reports.get(i);
-            if (report.size() <= 2) {
-                safeReports++;
-                continue;
-            }
-
-            boolean increasing = true;
-            int prev, dif, absDif, curr;
-            prev = report.get(0);
-
-            for (int j = 1; j < report.size(); j++) {
-
-                curr = report.get(j);
-                dif = prev - curr;
-                absDif = Math.abs(dif);
-
-                if ((absDif < 1) || (absDif > 3)) {
-                    break;
-                }
-
-                if (j == 1) {
-
-                    if (dif > 0) {
-                        increasing = false;
-                    }
-                } else {
-
-                    if ((increasing && (dif > 0) || (!increasing && (dif < 0)))) {
-                        break;
-                    }
-
-                    if (j == (report.size() - 1)) {
-                        safeReports++;
-                    }
-                }
-
-                prev = report.get(j);
-            }
-        }
-
-        return safeReports;
+        return solvePartTwo();
     }
 
     private static List<List<Integer>> extractReports(List<String> input) {
@@ -70,5 +23,105 @@ public class Day2 {
             reports.add(report);
         }
         return reports;
+    }
+
+    private static Integer solvePartOne() {
+        List<String> input = InputReader.textFile("data/day2.txt");
+        List<List<Integer>> reports = extractReports(input);
+        int safeReports = 0;
+        List<Integer> report;
+        for (int i = 0; i < reports.size(); i++) {
+            report = reports.get(i);
+            if (report.size() <= 2) {
+                safeReports++;
+                continue;
+            }
+            boolean increasing = true;
+            int prev, dif, absDif, curr;
+            prev = report.get(0);
+            for (int j = 1; j < report.size(); j++) {
+                curr = report.get(j);
+                dif = prev - curr;
+                absDif = Math.abs(dif);
+                if ((absDif < 1) || (absDif > 3)) {
+                    break;
+                }
+                if (j == 1) {
+                    if (dif > 0) {
+                        increasing = false;
+                    }
+                } else {
+                    if ((increasing && (dif > 0) || (!increasing && (dif < 0)))) {
+                        break;
+                    }
+                    if (j == (report.size() - 1)) {
+                        safeReports++;
+                    }
+                }
+                prev = report.get(j);
+            }
+        }
+        return safeReports;
+    }
+
+    private static Integer solvePartTwo() {
+        List<String> input = InputReader.textFile("data/day2.txt");
+        List<List<Integer>> reports = extractReports(input);
+        int safeReports = 0;
+        List<Integer> report;
+        for (int i = 0; i < reports.size(); i++) {
+            report = reports.get(i);
+            if (safeReportWithBadLevel(report)) {
+                safeReports++;
+            }
+        }
+        return safeReports;
+    }
+
+    private static boolean safeReport(List<Integer> report) {
+        int prev, dif, absDif, curr;
+        prev = report.get(0);
+        boolean increasing = true;
+        for (int j = 1; j < report.size(); j++) {
+            curr = report.get(j);
+            dif = prev - curr;
+            absDif = Math.abs(dif);
+            if ((absDif < 1) || (absDif > 3)) {
+                return false;
+            }
+            if (j == 1) {
+                if (dif > 0) {
+                    increasing = false;
+                }
+            } else {
+                if ((increasing && (dif > 0) || (!increasing && (dif < 0)))) {
+                    return false;
+                }
+                if (j == (report.size() - 1)) {
+                    return true;
+                }
+            }
+            prev = report.get(j);
+        }
+        return false;
+    }
+
+    private static boolean safeReportWithBadLevel(List<Integer> report) {
+        if (safeReport(report)) {
+            return true;
+        }
+        else return safeReportIfRemoved(report);
+    }
+
+    private static boolean safeReportIfRemoved(List<Integer> report) {
+        List<Integer> reducedReport;
+        for (int x = 0; x < report.size(); x++) {
+            reducedReport = new ArrayList<Integer>(report);
+            reducedReport.remove(x);
+            if (safeReport(reducedReport)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
