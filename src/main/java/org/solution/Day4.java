f6670b3f851c2e859260ca7c11216e523d1b3c3c
Collin Lacoursiere <61947934+cl16@users.noreply.github.com>
2024-12-24 13:19:39 -0500

    clean up day 4 solution

--- src/main/java/org/solution/Day4.java
+++ src/main/java/org/solution/Day4.java
@@ -1,39 +1,16 @@
 package org.solution;
 
 import org.helper.InputReader;
-import org.helper.XYCoordinates;
 
 import java.util.List;
 
 public class Day4 {
 
-    /** Notes on approach:
-     *
-     * Convert the puzzle text into a singular string with an integer representing the index of each character.
-     *
-     * For each "X" root of "XMAS", call 8 recursive functions that branch out in each of the 8 directions an
-     * XMAS string can go in.
-     *
-     * Recursive function will keep track of which character of XMAS it is checking for at a given index, and will
-     * keep track of whether it has gone "out of bounds".
-     *
-     * Use integer division and modulus to determine row/column of each integer for boundary navigation.
-     *
-     * OR...
-     *
-     * Can use X,Y coordinates and each trace in each direction can be broken into a pair of X-shift, Y-shift, where
-     * the X/Y shift can be easily determined to be out-of-bounds if in the first row (no Y+1), right-most column
-     * (no X+1), etc...
-     *
-     * Can index into the 2-D array of characters with X/Y indices easily too, instead of one long string...
-     */
-
     private int count;
-    private List<String> puzzleTextLines;
-    private int numRows;
-    private int numCols;
-    private String puzzleText;
-    private String target;
+    private final List<String> puzzleTextLines;
+    private final int numRows;
+    private final int numCols;
+    private final String target;
 
     public Day4() {
         count = 0;
@@ -64,7 +41,7 @@ public class Day4 {
         x = x + xShift;
         y = y + yShift;
         if ((x < 0) || (x == this.numCols) || (y < 0) || (y == this.numRows)) {
-            ;
+            ; // path is invalid, out of bounds
         }
         else if (wordIndex == (this.target.length() - 1)) {
             if (validTargetLetter(x, y, wordIndex)) {
